586809cc84ad36733f80b1fb5bc2950475c7ed28
hithaishi-25 <g.hithaishi3818@gmail.com>
2024-06-08 14:38:52 +0530

    Footer is added

--- my-app/src/Components/Footer.jsx
+++ my-app/src/Components/Footer.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-function Footer() {
-  return (
-    <div></div>
-  )
+const Footer = () => {
+    return (
+        <footer className="footer mt-9 bg-blue-900 text-blue-100 px-4 py-10">
+          <div className='flex flex-wrap justify-center text-center'>
+              <ul className='flex flex-col md:flex-row gap-4 md:gap-5 text-center md:text-left w-full md:w-auto onhover:text-blue-100 '>
+                <li className='hover:font-bold cursor-pointer'>Home</li>
+                <li className='hover:font-bold cursor-pointer'>About us</li>
+                <li className='hover:font-bold cursor-pointer'>Learning Science</li>
+                <li className='hover:font-bold cursor-pointer'>Learning Vedic Maths</li>
+                <li className='hover:font-bold cursor-pointer'>Learning Bespoke</li>
+              </ul>
+          </div>
+          
+          <div className='text-center mt-9'>© 2024 Credenza. All rights reserved.</div>
+          <div className='text-center'>Contact Us: g.hithaishi3818@gmail.com@yourcompany.com</div>
+        </footer>
+    );
 }
 
-export default Footer
+export default Footer;
